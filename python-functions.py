4a20079446d66ec1c9028e3a3f59f0e10afd31cc
djrich87 <djrich87@gmail.com>
2022-04-07 10:46:00 -0600

    finished lab

--- python-functions.py
+++ python-functions.py
@@ -8,31 +8,31 @@ def sum_to(num):
 
 
 #2.
-  def largest(nums):
-  largest = 0
-  for num in nums:
-    if num > largest:
-      largest = num
-  return largest
+#   def largest(nums):
+#   largest = 0
+#   for num in nums:
+#     if num > largest:
+#       largest = num
+#   return largest
   
-def largest(nums):
-  nums.sort()
-  return nums[-1]
+# def largest(nums):
+#   nums.sort()
+#   return nums[-1]
 
 
 #3.
-def occurances(string, substr):
-  stripped_string = string.replace(substr, '')
-  return (len(string) - len(stripped_string)) // len(substr)
+# def occurances(string, substr):
+#   stripped_string = string.replace(substr, '')
+#   return (len(string) - len(stripped_string)) // len(substr)
 	
-def occurances(string, substr):
-  return string.count(substr)
+# def occurances(string, substr):
+#   return string.count(substr)
 
 
 
 #4.
-  def product(*args):
-  product = 1
-  for arg in args:
-    product *= arg
-  return product
+  # def product(*args):
+  # product = 1
+  # for arg in args:
+  #   product *= arg
+  # return product
